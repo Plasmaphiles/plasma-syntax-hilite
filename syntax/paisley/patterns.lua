2b3ddf208d446f8f5a5ca033391cd9d80a56022d
Zachary Westerman <westerman.zachary@gmail.com>
2023-12-11 13:19:55 -0600

    Add highlighting for 'define' pseudo-command

--- syntax/paisley/patterns.lua
+++ syntax/paisley/patterns.lua
@@ -22,7 +22,7 @@ patterns = {
 
 	--keywords
 	kwd_1 = {
-		pattern = {"for", "in", "if", "elif", "while", "delete", "break", "continue"}, --parser auto-detects if it's at a word boundary
+		pattern = {"for", "in", "if", "elif", "while", "delete", "break", "continue", "define"}, --parser auto-detects if it's at a word boundary
 		display = "keyword", --apply coloring from theme.keyword
 		scope = "normal", --change scope (so commands aren't highlighted)
 	},
